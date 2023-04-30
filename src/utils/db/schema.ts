30712c3407afe861e764b9f371ece4355af753e5
shawshankkumar <shashankkumarthakur@gmail.com>
2023-05-01 00:22:06 +0530

    feat: Delete movie

--- src/utils/db/schema.ts
+++ src/utils/db/schema.ts
@@ -10,6 +10,6 @@ export const movies = mysqlTable("movies", {
   movie_id: serial("movie_id").primaryKey(),
   movie_name: varchar("movie_name", { length: 255 }).notNull(),
   movie_description: text("movie_description"),
-  movie_poster_url: varchar("movie_description", { length: 255 }),
+  movie_poster_url: varchar("movie_poster_url", { length: 255 }),
   created_at: timestamp("created_at").notNull().defaultNow().onUpdateNow(),
 });
