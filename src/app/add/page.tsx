a504b51d37f65ebe3dc52a64f2d67ab75307bd2a
shawshankkumar <shashankkumarthakur@gmail.com>
2023-05-01 02:42:08 +0530

    fix: Remove unused text

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -74,7 +74,6 @@ export default async function Home() {
         </div>
       </form>
       <p className="text-center text-gray-500 text-xs">
-        &copy;2020 Acme Corp. All rights reserved.
       </p>
     </div>
   );
