528f08c886690e63c85053629a5848fe60a48672
shawshankkumar <shashankkumarthakur@gmail.com>
2023-05-01 01:30:44 +0530

    chore: Landing page

--- src/app/api/movie/route.ts
+++ src/app/api/movie/route.ts
@@ -1,40 +1,5 @@
-import { NextResponse } from "next/server";
 import { db } from "@/utils/db/client";
 import { movies } from "@/utils/db/schema";
-import { eq } from "drizzle-orm";
-
-//Revalidate data only every 10 seconds
-
-export const runtime = "edge"; // 'nodejs' is the default
-export const revalidate = 10;
-
-export async function GET() {
-  try {
-    const data = await db.select().from(movies);
-    return new Response(
-      JSON.stringify({
-        message: "Movies fetched",
-        data,
-        success: true,
-      }),
-      {
-        status: 200,
-      }
-    );
-  } catch (err) {
-    console.log(err);
-    return new Response(
-      JSON.stringify({
-        message: "Unable to fetch Movies",
-        success: false,
-        err,
-      }),
-      {
-        status: 500,
-      }
-    );
-  }
-}
 
 export async function POST(req: Request) {
   try {
@@ -45,7 +10,6 @@ export async function POST(req: Request) {
       movie_description,
       movie_poster_url,
     });
-    console.log(insertMovie);
     return new Response(
       JSON.stringify({
         message: "Movies added",
